44bd3e683f95f8f19aa76ece1a30c48fbd0cd35b
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2020-04-07 11:45:09 +0200

    oxygen quizz saves data

--- utils/jsonFhirConverter.js
+++ utils/jsonFhirConverter.js
@@ -71,12 +71,69 @@ export default class jsonFhirConverter {
       ]
     };
   }
-  static jsonFhirToStringSimplified(jsonFile) {
-    return (
-      "Date: " +
-      jsonFile.authored +
-      "\nScore: " +
-      jsonFile.item[0].item[5].answer[0].valueInteger
-    );
+  static createOxygenQuestionnaireResponse(responses) {
+    let responsesObject = JSON.parse(responses);
+    return {
+      resourceType: "QuestionnaireResponse",
+      status: "completed",
+      authored: new Date(),
+      sentToServer: false,
+      item: [
+        {
+          linkId: "1",
+          text: "Oxygen Saturation Algorithm",
+
+          item: [
+            {
+              linkId: "1.1",
+              text: "Altitude (meters)",
+              answer: [
+                {
+                  valueInteger: responsesObject[0]
+                }
+              ]
+            },
+            {
+              linkId: "1.2",
+              text: "Saturation (%)",
+              answer: [
+                {
+                  valueInteger: responsesObject[1]
+                }
+              ]
+            },
+            {
+              linkId: "1.3",
+              text: "Result",
+              answer: [
+                {
+                  valueInteger: responsesObject[2]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+  static jsonFhirToStringSimplified(idQuizz, jsonFile) {
+    switch (idQuizz) {
+      case 95:
+        return (
+          "Date: " +
+          jsonFile.authored +
+          "\nScore: " +
+          jsonFile.item[0].item[5].answer[0].valueInteger
+        );
+      case 100:
+        return (
+          "Date: " +
+          jsonFile.authored +
+          "\nScore: " +
+          jsonFile.item[0].item[2].answer[0].valueInteger
+        );
+      default:
+        return "";
+    }
   }
 }
